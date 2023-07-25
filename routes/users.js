1a57e5b2ce72d202d095bc6d693269c1f97d1ba2
Eduardo de Campos da Silva <educampos4121@gmail.com>
2023-07-25 10:37:22 -0300

    beach e ifc

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,19 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+/* GET users listing. */
+router.get("/cool", function (req, res, next) {
+  res.send("you are so cool");
+});
+
+/* GET users listing. */
+router.get("/cool/beach", function (req, res, next) {
+  res.send("yo amo plaia");
+});
+
+/* GET users listing. */
+router.get("/ifc", function (req, res, next) {
+  res.send("welcome to high school");
+});
+
 module.exports = router;
